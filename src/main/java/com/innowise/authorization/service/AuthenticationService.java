5a0bca2888d48448c91490a0aae41079fd611393
LizavetaLiakh <lizaveta.liakh@innowise.com>
2025-11-07 18:45:35 +0300

    Fix service.

--- src/main/java/com/innowise/authorization/service/AuthenticationService.java
+++ src/main/java/com/innowise/authorization/service/AuthenticationService.java
@@ -3,6 +3,8 @@ package com.innowise.authorization.service;
 import com.innowise.authorization.entity.AuthenticationUser;
 import com.innowise.authorization.entity.Role;
 import com.innowise.authorization.repository.AuthenticationUserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
